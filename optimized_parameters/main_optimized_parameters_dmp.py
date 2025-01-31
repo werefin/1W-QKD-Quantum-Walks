0855edc431f56bf8dee0347a8fb6d74133423e56
David Polzoni <48173952+werefin@users.noreply.github.com>
2025-01-31 18:52:30 +0100

    Update __init__

--- optimized_parameters/main_optimized_parameters_dmp.py
+++ optimized_parameters/main_optimized_parameters_dmp.py
@@ -5,9 +5,8 @@ import subprocess
 import sys
 # Add the project root directory to Python's search path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from c_analysis import QW_Circle as QW_Circle_c, QW_Hypercube as QW_Hypercube_c
-from qer_analysis import Noise_Models
-from qer_analysis.qkd_protocol import QKD_Protocol_QW
+from c_analysis import QW_Circle_c, QW_Hypercube_c
+from qer_analysis import Noise_Models, QKD_Protocol_QW, QW_Circle, QW_Hypercube
 
 # Clone the private repository
 GITHUB_USERNAME = "Werefin"
