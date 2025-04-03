ad6f95b6bfe8fc6d41a5eca13d8d8879e89fc542
David Polzoni <48173952+werefin@users.noreply.github.com>
2025-04-03 18:25:13 +0200

    Update plot_qer_results_dn.py

--- plot_scripts/plot_qer_results_dn.py
+++ plot_scripts/plot_qer_results_dn.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # Define the path to the non-root user's plot folder
-normal_user = "david"
+normal_user = "normal_user"
 repo_dir = f"/home/{normal_user}/Desktop/plot"
 
 # Path to the JSON file
